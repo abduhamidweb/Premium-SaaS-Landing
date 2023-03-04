80538315059dbb12f2209b43c3326b3bc29fb64f
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-03-04 12:29:42 +0500

    Firsi commit. in the prect added Mt-U, bootstrap, bootstrap icons, redux/toolkit for front-end

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import React from 'react';
+import { Counter } from "./features/counter/Counter";
+
+const App = () => {
+  return (
+    <>
+      <Counter />
+    </>
+  );
+};
+
+export default App;
