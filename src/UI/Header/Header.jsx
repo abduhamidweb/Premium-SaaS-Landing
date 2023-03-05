9005dc29fe17fe328bc5f653b82ab24690042466
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-03-05 20:13:55 +0500

    header and responsive  end

--- src/UI/Header/Header.jsx
+++ src/UI/Header/Header.jsx
@@ -1,29 +1,36 @@
 import React from 'react';
-
+import logo from "../../assets/logo.png";
+import "./header.scss";
 const Header = () => {
     return (
         <>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar w / text</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary py-3">
+                <div className="container">
+                    <a className="navbar-brand" href="#">
+                        <img src={logo} alt="logo img" />
+                    </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <i className="bi bi-list  text-light"></i>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarText">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Features</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">About</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Pricing</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Features</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Pricing</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Blog</a>
                             </li>
                         </ul>
-                        <span class="navbar-text">
-                            Navbar text with an inline element
-                        </span>
+                        <a href='#' className='btn-oke'>Get a demo</a>
                     </div>
                 </div>
             </nav>
