7dc108418dbd6435fdebcde0eab0d3d00f010829
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-03-05 20:34:47 +0500

    hero end

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./hero.scss"
+import hero from "../../assets/hero.png";
+const Hero = () => {
+    return (
+        <>
+            <section className='hero'>
+                <div className="container">
+                    <div className="hero_content">
+                        <h2>A CRM dashboard for engineering teams</h2>
+                        <p>Rorem ipsum dolor sit amet consectetur. Gravida convallis orci ultrices non. Ultricies tempor at ut cursus mi. Aliquam sed amet vitae orci ac penatibus consectetur.</p>
+                        <div className="hero_links">
+                            <a href="#" className='btn-oke'> Get a demo</a>
+                            <a href="#" className='view-oke'>View pricing</a>
+                        </div>
+                        <div className="hero_view">
+                            <img src={hero} className='img-fluid' alt="img" />
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default Hero;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Router from "../../routes"
 import Header from '../../UI/Header/Header';
 import Footer from '../../UI/Footer/Footer';
+import Hero from '../../components/Hero/Hero';
 const Home = () => {
     return (
         <>
             <Header />
+            <Hero/>
             <Router />
             <Footer />
         </>
