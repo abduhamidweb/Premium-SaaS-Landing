2554bdff0d86e59b81b49443bd0acb7cb2ae6646
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-03-05 18:18:39 +0500

    oke header

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import "./App.scss";
 import React from 'react';
 import { Counter } from "./features/counter/Counter";
 import Home from "./pages/Home/Home";
-
 const App = () => {
   return (
     <>
       {/* <Counter  /> */}
-      <Home/>
+      <Home />
     </>
   );
 };
