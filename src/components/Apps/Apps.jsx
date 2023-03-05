29e9f87c9a2250e9e5fedf613b1ef3c418e11a35
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-03-05 21:11:03 +0500

    Apps start

---
+++ src/components/Apps/Apps.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./apps.scss";
+const Apps = () => {
+    return (
+        <div>
+            <h2>Appps</h2>
+        </div>
+    );
+};
+
+export default Apps;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,12 +4,14 @@ import Header from '../../UI/Header/Header';
 import Footer from '../../UI/Footer/Footer';
 import Hero from '../../components/Hero/Hero';
 import Service from '../../components/Service/Service';
+import Apps from '../../components/Apps/Apps';
 const Home = () => {
     return (
         <>
             <Header />
             <Hero />
             <Service />
+            <Apps />
             <Router />
             <Footer />
         </>
