8893ec48fd181a2da160c2d78ce97fae04510948
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-03-05 18:19:27 +0500

    ok header

--- src/UI/Header/Header.jsx
+++ src/UI/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
         <>
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar w/ text</a>
+                    <a class="navbar-brand" href="#">Navbar w / text</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
