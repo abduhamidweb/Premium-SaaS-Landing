d8352d3fb51fe1c7aa61aad8f9065520f76e9ce5
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-03-05 20:37:37 +0500

    service page start

---
+++ src/components/Service/Service.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./service.scss";
+const Service = () => {
+    return (
+        <>
+            <h2>Service</h2>
+        </>
+    );
+};
+
+export default Service;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,11 +3,13 @@ import Router from "../../routes"
 import Header from '../../UI/Header/Header';
 import Footer from '../../UI/Footer/Footer';
 import Hero from '../../components/Hero/Hero';
+import Service from '../../components/Service/Service';
 const Home = () => {
     return (
         <>
             <Header />
-            <Hero/>
+            <Hero />
+            <Service />
             <Router />
             <Footer />
         </>
